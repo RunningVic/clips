f8e85bdf2244c54ec184a0a3b80f8ce415e1592e
vic <yzhang3674@gatech.edu>
2023-06-06 16:35:22 -0400

    upload video complete

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'; 
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-upload',
@@ -8,8 +11,43 @@ import { Component } from '@angular/core';
 export class UploadComponent {
 
   isDragOver = false;
+  file: File | null = null;
+  nextStep = false;
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ],
+    nonNullable: true
+  });
+
+
+  uploadForm = new FormGroup({
+    title: this.title
+  });
+
+  constructor(
+    private storage: AngularFireStorage
+  ) {}
+
 
   storeFile($event: Event) {
     this.isDragOver = false;
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    // mime type type/subtype
+    if(!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
+    this.nextStep = true;
+  }
+
+  uploadFile() {
+    const clipFileName = uuid();
+    // firebase does not check duplicate file
+    // so we use uuid to generate unique id
+    const clipPath = `clips/${clipFileName}.mp4`;
+    this.storage.upload(clipPath, this.file);
   }
 }
