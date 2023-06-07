5f98b3f7e22f69c3dc73ec089ab013e294247b17
vic <yzhang3674@gatech.edu>
2023-06-06 23:37:54 -0400

    enabel upload cancel

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms'; 
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
 import { last, switchMap } from 'rxjs/operators';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
@@ -12,7 +12,7 @@ import { ClipService } from 'src/app/services/clip.service';
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
-export class UploadComponent {
+export class UploadComponent implements OnDestroy {
 
   isDragOver = false;
   file: File | null = null;
@@ -24,6 +24,7 @@ export class UploadComponent {
   percentage = 0;
   showPercentage = false;
   user: firebase.User | null = null;
+  task?: AngularFireUploadTask;
 
 
   title = new FormControl('', {
@@ -77,13 +78,13 @@ export class UploadComponent {
     // firebase does not check duplicate file
     // so we use uuid to generate unique id
     const clipPath = `clips/${clipFileName}.mp4`;
-    const task = this.storage.upload(clipPath, this.file);
+    this.task = this.storage.upload(clipPath, this.file);
     const clipRef = this.storage.ref(clipPath);
-    task.percentageChanges().subscribe(progress => {
+    this.task.percentageChanges().subscribe(progress => {
       this.percentage = (progress as number) / 100
     });
 
-    task.snapshotChanges().pipe(
+    this.task.snapshotChanges().pipe(
       last(), // only accpet last push
       switchMap(() => clipRef.getDownloadURL())
     ).subscribe({
@@ -110,5 +111,9 @@ export class UploadComponent {
         console.error(error);
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.task?.cancel();
   }
 }
