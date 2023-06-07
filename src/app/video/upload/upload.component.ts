6d1149ac9d9a9db68d639c54862c64fcc9617e53
vic <yzhang3674@gatech.edu>
2023-06-06 23:14:23 -0400

    disable form while upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -63,6 +63,7 @@ export class UploadComponent {
   }
 
   uploadFile() {
+    this.uploadForm.disable();
     this.showAlert = true;
     this.alertColor = 'blue';
     this.alertMsg = 'Please wait! Your clip is being uploaded.';
@@ -98,6 +99,7 @@ export class UploadComponent {
         this.showPercentage = false;
       },
       error: (error) => {
+        this.uploadForm.enable();
         this.alertColor = 'red';
         this.alertMsg = 'Upload failed! Please try again later';
         this.inSubmission = true;
